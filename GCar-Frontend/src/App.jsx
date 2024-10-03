8e06581c83741ecfb435e5cc95e73aa3e33f9ad5
Ashton <144487285+dyashton@users.noreply.github.com>
2024-10-03 14:39:39 -0400

    Created all individual components

--- GCar-Frontend/src/App.jsx
+++ GCar-Frontend/src/App.jsx
@@ -2,24 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
-import Card from '../Components/Card/Card'
 import VideoStream from '../Components/VideoStream/VideoStream'
 import FrameDiagnostic from '../Components/FrameDiagnostic/FrameDiagnostic'
 import { delay, motion } from 'framer-motion'
+import Controls from '../Components/Card/Controls/Controls'
+import IMUInfo from '../Components/Card/IMUInfo/IMUInfo'
+import DataStream from '../Components/Card/DataStream/DataStream'
+import Instruction from '../Components/Card/Instruction/Instruction'
 
 function App() {
 
   return (
     <div className="MainCont">
-      <div className="FirstRow">
-        <FrameDiagnostic id="FrameDiagnostic"/>
-          <Card id="IMUInfo" title={"IMU Information"} index={1}/>
-        <VideoStream id="VideoStream"/>
+      <div className="FirstColumn">
+        <div className="FrameDataCont">
+          <FrameDiagnostic id="FrameDiagnostic"/>
+          <DataStream id="DataStream"/>
+        </div>
+        <IMUInfo id="IMUInfo"/>
+
       </div>
-      <div className="SecondRow">
-        <Card id="CurrentInstructions" title={"Current Instructions"} index={2}/>
-        <Card id="Controls" title={"Controls"} index={3}/>
-        <Card id="DataStream" title={"Data Stream"} index={4}/>
+      <div className="SecondColumn">
+        <VideoStream id="VideoStream"/>
+        <div className="InstructControlsCont">          
+          <Instruction id="Instruction" />
+          <Controls id="Controls"/>
+        </div>
       </div>
     </div>
   )
