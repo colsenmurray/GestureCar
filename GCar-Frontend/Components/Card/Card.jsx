2ddf05d20c94fbfeb66feb69a0974094941723fe
Ashton <144487285+dyashton@users.noreply.github.com>
2024-10-02 20:47:35 -0400

    added animations

--- GCar-Frontend/Components/Card/Card.jsx
+++ GCar-Frontend/Components/Card/Card.jsx
@@ -1,12 +1,20 @@
 import styles from "./Card.module.scss";
+import { motion } from 'framer-motion'
 
-
-function Card({ title, className, id }) {
+function Card({ title, className, id, index }) {
 
     return (
-        <div className={`${styles.CardCont}`} id={id}>
-            {title}
-        </div>
+        <motion.div className={`${styles.CardCont}`} id={id}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.2 , ease: "easeInOut", duration: .75 }}
+        >
+            <motion.div className={`${styles.Title}`}
+            initial={{ filter: "drop-shadow(0px 0px 10px $tertiary)" }}
+            >
+                {title}
+            </motion.div>
+        </motion.div>
     )
 }
 
