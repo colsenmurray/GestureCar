3d5187a861b009ad749073aa04c9b0ac74ff5fea
Colsen <166042861+ColsenUTK@users.noreply.github.com>
2024-11-22 02:01:57 -0500

    created socketio

--- dev/server.py
+++ dev/server.py
@@ -1,22 +1,37 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 import eventlet
 import time
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+CORS(app,resources={r"/*":{"origins":"*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")  # Allow any frontend to connect
+
+send_data_flag = True
+
+def send_data():
+    i = 0
+    while send_data_flag:
+        print("Sending Data...")
+        socketio.emit('server_event', {'data': 'Hello from Flask! ' + str(i)})
+        i += 1
+        time.sleep(2)
 
 @socketio.on('connect')
 def handle_connect():
-    print("Client Connected...")
-
-    # emit data once connected
-    def send_data():
-        while True:
-            socketio.emit('server_event', {'data': 'Hello from Python Flask!!'})
-            time.sleep(2)
-
+    print("Client connected")
     socketio.start_background_task(send_data)
+    global send_data_flag
+    send_data_flag = True
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global send_data_flag
+    print('Client Disconnected')
+    send_data_flag = False
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port = 5000)
+    eventlet.monkey_patch()
+
+    socketio.run(app, port=5001)
